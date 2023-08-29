046121c938cd43bb1cad782d931fb5767da35169
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-08-29 14:42:48 +0300

    Updated the categoriesSlice.js file

--- src/redux/categories/categoriesSlice.js
+++ src/redux/categories/categoriesSlice.js
@@ -1,13 +1,19 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-const initialState = {
-  categories: ['Under construction'],
-};
-
 const categoriesSlice = createSlice({
   name: 'categories',
-  initialState,
-  reducers: {},
+  initialState: {
+    categories: [],
+  },
+  reducers: {
+    checkStatus: (state, action) => {
+      state.categories = action.payload === 'Under construction'
+        ? 'Under construction'
+        : state.categories;
+    },
+  },
 });
+
+export const { checkStatus } = categoriesSlice.actions;
 
 export default categoriesSlice.reducer;
