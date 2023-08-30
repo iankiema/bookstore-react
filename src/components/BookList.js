5644af919d35161beda74bc61b6e4f082c06a96e
Ian Kiema <112932127+iankiema@users.noreply.github.com>
2023-08-30 11:27:53 +0300

    Updated the components folder

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,20 +1,34 @@
 // BookList.js
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeBook } from '../redux/books/booksSlice';
+import { removeBook, fetchBooks } from '../redux/books/booksSlice';
 
 const BookList = () => {
   const books = useSelector((state) => state.books.books);
+  const status = useSelector((state) => state.books.status);
   const dispatch = useDispatch();
+
+  const appId = 'abc123';
 
   const handleRemoveBook = (itemId) => {
     dispatch(removeBook(itemId));
   };
 
+  useEffect(() => {
+    if (status === 'idle') {
+      dispatch(fetchBooks(appId));
+    }
+  }, [status, dispatch, appId]);
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      {books.map((book) => (
-        <div className="book" key={book.item_id}>
+      {Object.keys(books).map((item_id) => (
+        <div className="book" key={item_id}>
+          {books[item_id].map(book) => (
           <h3>{book.title}</h3>
           <p className="book-author">
             Author:
@@ -29,7 +43,9 @@ const BookList = () => {
           <button className="book-delete-button" type="button" onClick={() => handleRemoveBook(book.item_id)}>
             Remove Book
           </button>
+          )}
         </div>
+
       ))}
     </div>
   );
--- src/redux/books/booksSlice.js
+++ src/redux/books/booksSlice.js
@@ -1,27 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+/* eslint-disable no-param-reassign */
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 const initialState = {
-  books: [
-    {
-      item_id: 'item1',
-      title: 'The Great Gatsby',
-      author: 'John Smith',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item2',
-      title: 'Anna Karenina',
-      author: 'Leo Tolstoy',
-      category: 'Fiction',
-    },
-    {
-      item_id: 'item3',
-      title: 'The Selfish Gene',
-      author: 'Richard Dawkins',
-      category: 'Nonfiction',
-    },
-  ],
+  books: [],
+  status: 'idle',
+  error: null,
 };
+
+export const fetchBooks = createAsyncThunk('books/fetchBooks', async (app_id) => {
+  const response = await axios.get(`https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/${app_id}/books`);
+  return response.data;
+});
 
 const booksSlice = createSlice({
   name: 'books',
@@ -31,9 +21,22 @@ const booksSlice = createSlice({
       state.books.push(action.payload);
     },
     removeBook: (state, action) => {
-      // eslint-disable-next-line no-param-reassign
       state.books = state.books.filter((book) => book.item_id !== action.payload);
     },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchBooks.pending, (state) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchBooks.fulfilled, (state, action) => {
+        state.status = 'succeeded';
+        state.books = action.payload;
+      })
+      .addCase(fetchBooks.rejected, (state, action) => {
+        state.status = 'failed';
+        state.error = action.error.message;
+      });
   },
 });
 
